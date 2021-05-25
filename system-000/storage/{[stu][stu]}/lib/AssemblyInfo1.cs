a601b165d0163264c4cc5e12e0d799184493b14e
GokalpSen <gokalpsen09@gmail.com>
2021-05-25 12:16:15 +0300

    Proje dosyası ekle.

---
+++ system-000/storage/{[stu][stu]}/lib/AssemblyInfo1.cs
@@ -0,0 +1,18 @@
+using System.Runtime.InteropServices;
+
+// Bunun gibi SDK stili projelerde, geçmişte bu dosya içinde tanımlanan birkaç bütünleştirilmiş
+// kod özniteliği, artık derleme sırasında otomatik olarak eklenir ve proje özelliklerinde
+// tanımlanan değerlerle doldurulur. Hangi özniteliklerin dahil edileceği ve bu işlemin
+// nasıl özelleştirileceği hakkında ayrıntılı bilgi için bkz. https://aka.ms/assembly-info-properties
+
+
+// ComVisible değerinin false olarak ayarlanması, bu bütünleştirilmiş koddaki türleri
+// COM bileşenlerine görünmez hale getirir. Bu bütünleştirilmiş koddaki bir türe COM'dan
+// erişmeniz gerekiyorsa, ComVisible özniteliğini bu türde true olarak ayarlayın.
+
+[assembly: ComVisible(false)]
+
+// Aşağıdaki GUID, bu projenin COM'un kullanımına sunulması durumunda typelib'in kimliği
+// içindir.
+
+[assembly: Guid("aca9fab4-18f1-4983-b6d2-3d040f7f61ef")]
---
+++ system-000/storage/{[stu][stu]}/lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//storage path:::(system-000.storage/
+
+namespace system_000.storage.__stu__stu__.lib
+{
+    class Class1
+    {
+    }
+}
