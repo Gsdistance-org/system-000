77ab7de8cb0b027ef47f066593d055178d7f8b7c
GokalpSen <gokalpsen09@gmail.com>
2021-05-28 15:33:24 +0300

    bcf

---
+++ system-000/dll/library/calc/calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace system_000.dll.library.calc
+{
+    public static class calc
+    {
+        public static double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
