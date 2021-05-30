49db06f00ca5419e93e61cdf9cf451f3d99f246b
GokalpSen <gokalpsen09@gmail.com>
2021-05-30 16:25:48 +0300

    t

---
+++ system-000/dll/game/mapper/Setup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// this is the parser setup for game mapping to start the parser you will need to know how this works
+/// </summary>
+namespace system_000.dll.game.mapper
+{
+    public class Setup
+    {
+        public static double start(double name, double extra)
+        {
+            var timeelapse = 1;
+            Console.WriteLine("Setting up mapper parser for" + name);
+            Console.WriteLine(".");
+            Console.WriteLine("..");
+            Console.WriteLine("...");
+            Console.WriteLine("....");
+            Console.WriteLine("Done in"+ timeelapse);
+        }
+    }
+}
