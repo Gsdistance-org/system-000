9d9ec778514b1e33a791f2765e9d60ce9ad5574c
GokalpSen <gokalpsen09@gmail.com>
2021-06-01 13:09:52 +0300

    dgr

---
+++ system-000/sys/000/Setup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace system_000.sys._000
+{
+    class Setup
+    {
+    }
+}
